c6029cbecc24582c511edf842eb0306ab609ff27
AnjaliRekhate <75578601+AnjaliRekhate@users.noreply.github.com>
2022-07-06 15:17:52 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.20.1"
+    }
+  }
+  backend "s3" {
+    bucket = "tf-anjali"
+    key    = "states/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+
+}
+
