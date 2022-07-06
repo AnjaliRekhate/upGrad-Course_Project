67dd9179fb24fc45b285c12d59250d9dea0e000b
AnjaliRekhate <75578601+AnjaliRekhate@users.noreply.github.com>
2022-07-06 15:14:52 +0530

    Create bastionhost-sg.tf

---
+++ bastionhost-sg.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "bastion_SG" {
+  name        = "Bastion_host_SG"
+  description = "security group using Terraform"
+  vpc_id      =  module.vpc.vpc_id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+    }
