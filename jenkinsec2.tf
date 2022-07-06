7e2dfe39fa641dbd10e247346771f06d24d1e7a1
AnjaliRekhate <75578601+AnjaliRekhate@users.noreply.github.com>
2022-07-06 15:15:40 +0530

    Create jenkinsec2.tf

---
+++ jenkinsec2.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ubuntu3" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.ubuntu3.id
+  instance_type = "t2.micro"
+  key_name      = "demo"
+  vpc_security_group_ids = [aws_security_group.private_SG.id]
+  subnet_id = module.vpc.private_subnets[0]
+
+tags = {
+    Name = "JenkinsEC2"
+
+}
+    }
